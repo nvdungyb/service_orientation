a4a767d93ae067841c0b17686d4b1722412118d6
NvDzung <dung3032003md@gmail.com>
2025-05-20 17:26:06 +0700

    feat: add Feign client for fetching post by ID from post service

---
+++ services/moderation-service/src/main/java/com/java/moderationservice/http/PostClient.java
@@ -0,0 +1,14 @@
+package com.java.moderationservice.http;
+
+import com.java.postservice.dto.ApiResponse;
+import com.java.postservice.dto.response.PostResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "post-client", url = "${post.service.url}")
+public interface PostClient {
+    @GetMapping("/posts/{id}")
+    ResponseEntity<ApiResponse<PostResponseDto>> getPostById(@PathVariable("id") Long postId);
+}
---
+++ services/moderation-service/src/main/java/com/java/moderationservice/http/PostServiceClient.java
@@ -0,0 +1,39 @@
+package com.java.moderationservice.http;
+
+import com.java.postservice.dto.ApiResponse;
+import com.java.postservice.dto.response.PostResponseDto;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class PostServiceClient {
+
+    private final PostClient postClient;
+
+    public PostServiceClient(PostClient postClient) {
+        this.postClient = postClient;
+    }
+
+    // todo: chưa ổn.
+
+    /**
+     * @param postId
+     * @return PostResponseDto if data is present and Post service is not interrupted otherwise Optional.empty()
+     */
+    public Optional<PostResponseDto> getPostById(Long postId) {
+        try {
+            ResponseEntity<ApiResponse<PostResponseDto>> response = postClient.getPostById(postId);
+            return Optional.ofNullable(response.getBody().getData());
+        } catch (FeignException.FeignClientException ex) {
+            log.error(ex.getMessage());
+        } catch (FeignException ex) {
+            log.error(ex.getMessage());
+        }
+        return Optional.empty();
+    }
+}
